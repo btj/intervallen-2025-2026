74d41039ce4a4ae13922f045687e1b3d1dee545a
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2026-02-16 11:03:37 +0100

    Vervolledig + stappenplan

--- intervallen/src/intervallen/Interval.java
+++ intervallen/src/intervallen/Interval.java
@@ -1,23 +1,68 @@
 package intervallen;
 
+/**
+ * Elke instantie van deze klasse slaat een
+ * interval met gehele ondergrens en gehele
+ * bovengrens op.
+ * 
+ * @invar De lengte is niet negatief.
+ *   | 0 <= getLength()
+ * @invar
+ *   | getLength() == getBovengrens() - getOndergrens()
+ */
 public class Interval {
+	/**
+	 * @invar | 0 <= lengte
+	 */
 	private int ondergrens;
 	private int lengte;
-	
-	Interval(int ondergrens, int bovengrens) {
-		this.ondergrens = ondergrens;
-		this.lengte = bovengrens - ondergrens;
-	}
 
-	void setOndergrens(int ondergrens) {
-		this.ondergrens = ondergrens;
+	public int getOndergrens() { return ondergrens; }
+	public int getBovengrens() {
+		return ondergrens + lengte;
 	}
-	
-	void setBovengrens(int bovengrens) {
-		this.lengte = bovengrens - this.ondergrens;
-	}
-
-	int getLength() {
+	public int getLength() {
 		return this.lengte;
+	}
+	
+	/**
+	 * Initialiseert het object met de gegeven
+	 * ondergrens en bovengrens.
+	 * @throws IllegalArgumentException
+	 *     | ondergrens > bovengrens
+	 * @post | getOndergrens() == ondergrens
+	 * @post | getLength() == bovengrens - ondergrens
+	 */
+	public Interval(int ondergrens, int bovengrens) {
+		if (ondergrens > bovengrens)
+			throw new IllegalArgumentException(
+					"`ondergrens` is groter dan" +
+			        "`bovengrens`");
+		this.ondergrens = ondergrens;
+		lengte = bovengrens - ondergrens;
+	}
+
+	/**
+	 * Stelt de ondergrens van het interval in op
+	 * de gegeven waarde.
+	 * 
+	 * @mutates | this
+	 * @post | getOndergrens() == ondergrens
+	 * @post | getLength() == old(getLength())
+	 */
+	public void setOndergrens(int ondergrens) {
+		this.ondergrens = ondergrens;
+	}
+	
+	/**
+	 * Stelt de bovengrens van het interval in op
+	 * de gegeven waarde.
+	 * @pre | getOndergrens() <= bovengrens
+	 * @mutates | this
+	 * @post | getBovengrens() == bovengrens
+	 * @post | getOndergrens() == old(getOndergrens())
+	 */
+	public void setBovengrens(int bovengrens) {
+		lengte = bovengrens - this.ondergrens;
 	}
 }
